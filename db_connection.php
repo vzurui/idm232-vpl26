5a14babc8173f52703d870b77ed1384ebcbae2a1
Vy Le <139026534+vzurui@users.noreply.github.com>
2024-11-28 16:43:56 -0500

    htaccess in

--- db_connection.php
+++ db_connection.php
@@ -1,18 +1,16 @@
 <?php
-$host = 'localhost';
-$username = 'root';
-$password = 'root';
-$database = 'idm232_db';
+// Load environment variables
+$host = getenv('DB_SERVER') ?: 'localhost';
+$username = getenv('DB_USERNAME') ?: 'root';
+$password = getenv('DB_PASSWORD') ?: 'root';
+$database = getenv('DB_NAME') ?: 'idm232_db';
 
-$conn = new mysqli($host, $password, $username, $database);
+// Create a connection
+$conn = new mysqli($host, $username, $password, $database);
 
+// Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully!";
-
-$conn->close();
 ?>
-
-
-
